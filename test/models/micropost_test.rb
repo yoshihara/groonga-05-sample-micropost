14ae778617a4073eb6a904fde8b9ef929ddc1c2d
yoshihara <h.yoshihara@everyleaf.com>
2014-11-21 17:02:45 +0900

    test: add Micropost model tests

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include DatabaseConfiguration
+
+  setup do
+    @content = 'test'
+    @micropost = FactoryGirl.build(:micropost, content: @content)
+  end
+
+  test 'should update index form saved micropost' do
+    @micropost.save!
+
+    assert do
+      MicropostIndex.microposts[@micropost.id.to_s].content == @content
+    end
+  end
 end

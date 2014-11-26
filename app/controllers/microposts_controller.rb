acaade00df3a884068cb04f8c8f01386eaf84643
yoshihara <h.yoshihara@everyleaf.com>
2014-11-26 21:04:34 +0900

    Remove needless comments

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,23 +1,17 @@
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: [:edit, :update, :destroy]
 
-  # GET /microposts
-  # GET /microposts.json
   def index
     @microposts = Micropost.all
   end
 
-  # GET /microposts/new
   def new
     @micropost = Micropost.new
   end
 
-  # GET /microposts/1/edit
   def edit
   end
 
-  # POST /microposts
-  # POST /microposts.json
   def create
     @micropost = Micropost.new(micropost_params)
 
@@ -28,8 +22,6 @@ class MicropostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /microposts/1
-  # PATCH/PUT /microposts/1.json
   def update
     if @micropost.update(micropost_params)
       redirect_to microposts_url, notice: 'Micropost was successfully updated.'
@@ -38,8 +30,6 @@ class MicropostsController < ApplicationController
     end
   end
 
-  # DELETE /microposts/1
-  # DELETE /microposts/1.json
   def destroy
     @micropost.destroy
     redirect_to microposts_url, notice: 'Micropost was successfully destroyed.'
@@ -47,12 +37,10 @@ class MicropostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_micropost
     @micropost = Micropost.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def micropost_params
     params.require(:micropost).permit(:content)
   end
